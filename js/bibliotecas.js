e0532f65c06be502b10275423217471f913d0d93
Alan Sánchez <alanes@comunidad.unam.mx>
2019-09-02 15:29:06 -0500

    js 100% funcional

--- js/bibliotecas.js
+++ js/bibliotecas.js
@@ -1,3 +1,41 @@
+criterio = 1;
+
+function num_casillas_activas(libro){
+	var cont = 0;
+	for(var i = 1;i<libro.classList.length;i++){
+		if(document.getElementById("cb"+libro.classList[i]).checked){
+			cont++;
+		}
+	}
+	return cont;
+}
+
+function casilla_activa(libro){
+	var cont = num_casillas_activas(libro);
+	
+	return cont>0?true:false;
+}
+
+function buscar(campo){
+	var input, filter, table, tr, td, i, valor_txt;
+	
+	input = document.getElementById("barra_busqueda");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("tabala_libros");
+	tr = document.getElementsByClassName("Biblioteca");
+	
+	for (i = 0; i < tr.length; i++) {
+		valor_txt = tr[i].children[criterio].textContent;
+		if(valor_txt.toUpperCase().indexOf(filter)>-1){
+				if(casilla_activa(tr[i])){
+					tr[i].style.display = "";
+				}
+		}else{
+			tr[i].style.display = "none";
+		}
+	}
+}
+
 function coloca_biblio(biblioteca){
 	var biblios = document.getElementsByClassName(biblioteca);
 	for(i of biblios){
@@ -8,7 +46,9 @@ function coloca_biblio(biblioteca){
 function quita_biblio(biblioteca){
 	var biblios = document.getElementsByClassName(biblioteca);
 	for(i of biblios){
-		i.style.display="none";
+		if(num_casillas_activas(i)==0){
+			i.style.display="none";
+		}
 	}
 }
 
@@ -26,7 +66,7 @@ function selcciona_todas(valor_check){
 	document.getElementById("cbBPIngenieria").checked = valor_check;
 	document.getElementById("cbBAIngenieria").checked = valor_check;
 	document.getElementById("cbBPosIngenieria").checked = valor_check;
-	document.getElementById("cbBNM").checked = valor_check;
+	document.getElementById("cbBBNM").checked = valor_check;
 	document.getElementById("cbBCiencias").checked = valor_check;
 	document.getElementById("cbBQuimica").checked = valor_check;
 }
@@ -52,6 +92,10 @@ function comprueba_eleccion(check_box,es_todas,biblioteca){
 function carga_eventos(){
 	selcciona_todas(true);
 
+	document.getElementById("barra_busqueda").value="";
+
+	document.getElementById("buscarPorTitulo").checked=true;
+
 	document.getElementById("cbBTodas").addEventListener("change", function(){
 		comprueba_eleccion(document.getElementById("cbBTodas"),true,"");
 	});
@@ -65,27 +109,51 @@ function carga_eventos(){
 	});
 
 	document.getElementById("cbBPIngenieria").addEventListener("change", function(){
-		comprueba_eleccion(document.getElementById("cbBPIngenieria"),false,"PIngenieria");
+		comprueba_eleccion(document.getElementById("cbBPIngenieria"),false,"BPIngenieria");
 	});
 
 	document.getElementById("cbBAIngenieria").addEventListener("change", function(){
-		comprueba_eleccion(document.getElementById("cbBAIngenieria"),false,"AIngenieria");
+		comprueba_eleccion(document.getElementById("cbBAIngenieria"),false,"BAIngenieria");
 	});
 	
 	document.getElementById("cbBPosIngenieria").addEventListener("change", function(){
-		comprueba_eleccion(document.getElementById("cbBPosIngenieria"),false,"PosIngenieria");
+		comprueba_eleccion(document.getElementById("cbBPosIngenieria"),false,"BPosIngenieria");
 	});
 
-	document.getElementById("cbBNM").addEventListener("change", function(){
-		comprueba_eleccion(document.getElementById("cbBNM"),false,"BNM");
+	document.getElementById("cbBBNM").addEventListener("change", function(){
+		comprueba_eleccion(document.getElementById("cbBNM"),false,"BBNM");
 	});
 
 	document.getElementById("cbBQuimica").addEventListener("change", function(){
-		comprueba_eleccion(document.getElementById("cbBQuimica"),false,"Quimica");
+		comprueba_eleccion(document.getElementById("cbBQuimica"),false,"BQuimica");
 	});
 
 	document.getElementById("cbBCiencias").addEventListener("change", function(){
-		comprueba_eleccion(document.getElementById("cbBCiencias"),false,"Ciencias");
+		comprueba_eleccion(document.getElementById("cbBCiencias"),false,"BCiencias");
+	});
+
+	document.getElementById("barra_busqueda").addEventListener("keyup",function(){
+		buscar(2);
+	});
+
+	document.getElementById("buscarPorTitulo").addEventListener("change",function(){
+		criterio=1;
+	});
+
+	document.getElementById("buscarPorAutor").addEventListener("change",function(){
+		criterio=0;
+	});
+
+	document.getElementById("buscarPorEditorial").addEventListener("change",function(){
+		criterio=2;
+	});
+
+	document.getElementById("buscarPorTemas").addEventListener("change",function(){
+		criterio=3;
+	});
+	
+	document.getElementById("buscarPorBiblioteca").addEventListener("change",function(){
+		criterio=4;
 	});
 }
 
